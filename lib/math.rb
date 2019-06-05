780f513b775602e465081c867e52e0dc5df81a2b
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-05 00:42:45 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
 def addition(num1, num2)
+  puts 5+4=9
 end
 
 def subtraction(num1, num2)
+  puts 10-5=5
 end
 
 def division(num1, num2)
+  puts 81/9
 end
 
 def multiplication(num1, num2)
+  puts 4*5
 end
 
 def modulo(num1, num2)
+  puts 5%3
 end
 
 def square_root(num)
+  puts Math.sqrt(6)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  puts 36+((12*2)/4)
 end
